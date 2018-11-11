3f6c8a5aaad8d6300664418b8d441bb35df4896d
JirkaZednik <44942005+jirkazednik@users.noreply.github.com>
2018-11-11 16:08:39 +0100

    Grid systém

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,31 @@ import { GatewayService } from './services/gateway.service';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="app_center">
-      <div style="height: 50%;">
+    <div class="container">
+    <div class="row">
+      <div class="col-md-2">
+       </div>
+      <div class="col-md-4">
         <leva-strana style="float: left;"></leva-strana>
+      </div>
+      <div class="col-md-4">
         <prava-strana style="float: left;">
           <input type="button" class="btn btn-success" value="Kontaktujte mě" [disabled]="zobrazFormular" (click)="this.zobrazFormular = !this.zobrazFormular">
         </prava-strana>
       </div>
-      <div style="height: 50%;">
+      <div class="col-md-2">
+        </div>
+      </div>
+    
+    <div class="row">
+      <div class="col-md-2">
+      </div>
+      <div class="col-md-8">
         <kontaktni-formular *ngIf="this.zobrazFormular"></kontaktni-formular>
       </div>
+      <div class="col-md-2">
+      </div>
+    </div>
     </div>
   `,
   styleUrls: ['./app.component.css']
