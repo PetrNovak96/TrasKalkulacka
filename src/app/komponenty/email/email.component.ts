20cd9017d821659dada6cab9a0cea3ed7d368c62
PetrNovak96 <novp19@vse.cz>
2018-11-18 21:42:44 +0100

    Pole a email jsou po kliku vyplněny defaultními hodnotami

--- src/app/komponenty/email/email.component.ts
+++ src/app/komponenty/email/email.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'email',
   template: `
     <p>
     Email
-    <input type="email" class="form-control" placeholder="@">
+    <input #emailTextField 
+           type="text" 
+           [(ngModel)]="this.email" 
+           class="form-control" 
+           placeholder="@" 
+           (click)="this.onClickEvent()">
     </p>
   `,
   styleUrls: ['./email.component.css']
 })
 export class EmailComponent implements OnInit {
 
+  public email: string;
+  @ViewChild('emailTextField') input;
+
   constructor() { }
 
   ngOnInit() {
+    this.email = "";
+  }
+
+  onClickEvent(){
+
+    if(this.email.length==0){
+      let el = this.input.nativeElement;
+      el.value = "@";
+      el.setSelectionRange(0,0)
+    }
   }
 
 }
--- src/app/komponenty/telefonni-cislo/telefonni-cislo.component.ts
+++ src/app/komponenty/telefonni-cislo/telefonni-cislo.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'telefonni-cislo',
   template: `
     <p>
       Telefonní číslo
-    <input type="tel" class="form-control" placeholder="+420 602 123 456">
+    <input #telCisloTextField
+           type="text" 
+           class="form-control" 
+           placeholder="+420 602 123 456" 
+           (click)="onClickEvent()" 
+           [(ngModel)]="this.telCislo">
     </p>
   `,
   styleUrls: ['./telefonni-cislo.component.css']
 })
 export class TelefonniCisloComponent implements OnInit {
 
+  public telCislo: string;
+  public pomocna: number;
+  @ViewChild('telCisloTextField') input;
+
   constructor() { }
 
   ngOnInit() {
+    this.telCislo = "";
+  }
+
+  onClickEvent(){
+
+    if(this.telCislo.length==0){
+      let el = this.input.nativeElement;
+      let newVal = "+420 ";
+      el.value = newVal;
+      this.pomocna = newVal.length;
+      el.setSelectionRange(newVal.length, newVal.length);
+    }
   }
 
 }
