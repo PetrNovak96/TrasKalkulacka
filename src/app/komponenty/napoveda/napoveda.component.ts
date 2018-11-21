b24b2801011a21e1b0c63db7e181995952cd5b1b
zedniji <jirka.zednik@gmail.com>
2018-11-21 13:19:21 +0100

    css

--- src/app/komponenty/napoveda/napoveda.component.ts
+++ src/app/komponenty/napoveda/napoveda.component.ts
@@ -5,14 +5,16 @@ import { Component, OnInit, Input } from '@angular/core';
   template: `
     <img [matTooltip]="this.tooltip"
          [matTooltipPosition]="this.pozice"
-         src="assets/info.png"
+         src="assets/help31b.png"
          alt="Nápověda"
          width="15px"
          height="15px"
     >
   `,
   styleUrls: ['./napoveda.component.css']
+
 })
+
 export class NapovedaComponent implements OnInit {
 
   @Input() public tooltip: string;
