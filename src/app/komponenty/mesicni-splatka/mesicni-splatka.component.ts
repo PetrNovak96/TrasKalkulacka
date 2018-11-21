b9899782a0e6b5a0ec8300d15f0468bdab18ee7c
zedniji <jirka.zednik@gmail.com>
2018-11-21 10:11:01 +0100

    text css style, rightside

--- src/app/komponenty/mesicni-splatka/mesicni-splatka.component.ts
+++ src/app/komponenty/mesicni-splatka/mesicni-splatka.component.ts
@@ -3,15 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @ Component({
   selector: 'mesicni-splatka',
   template: `
-    <p class="txtWhite">
+    <p class="txtWhite txtWeightNorm">
       Měsíční splátka
     </p>
     <p class="txtWhite"  style="font-size: 28px; margin-bottom: 25px" *ngIf="(vysledek != undefined)">
       {{this.numberToString(vysledek) + " Kč"}}
     </p>
-    <p *ngIf="jePojisteni">{{this.pojisteniInfo}}</p>
-    <p>U tohoto úvěru <b style="color: #00CC33">nepožadujeme</b> uvést jeho účel </p>
-    <p>a můžete ho kdykoliv předčasně splatit.</p>
+    <p *ngIf="jePojisteni" class="txtWhite txtWeightNorm">{{this.pojisteniInfo}}</p>
+    <p class="txtWhite txtWeightNorm">U tohoto úvěru <b style="color: #00CC33">nepožadujeme</b> uvést jeho účel </p>
+    <p class="txtWhite txtWeightNorm">a můžete ho kdykoliv předčasně splatit.</p>
     <hr color="white">
   `,
   styleUrls: ['./mesicni-splatka.component.css']
